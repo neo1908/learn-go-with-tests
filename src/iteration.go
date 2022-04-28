f9f744afa302c1a4cdde78d2f28e6ca15a6da43a
Ben <0x620x64@protonmail.com>
2022-04-28 13:13:20 +0100

    Finish off iteration chapter

--- src/iteration.go
+++ src/iteration.go
@@ -1,8 +1,10 @@
 package src
 
+// Repeat Can be replaced with strings.Repeat()
 func Repeat(character string, count int) string {
 	var repeated string
 
+	// There is only "for()" no "while" etc
 	for i := 0; i < count; i++ {
 		repeated += character
 	}
